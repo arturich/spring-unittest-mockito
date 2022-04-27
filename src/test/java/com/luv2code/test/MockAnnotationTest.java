950fd27bb1d8d098c56f65091e45f6233de32f5a
arturich <arturich@gmail.com>
2022-04-27 10:49:25 -0500

    Creating a test that creates multiple stubs with different responses using mocks with thenReturn

--- src/test/java/com/luv2code/test/MockAnnotationTest.java
+++ src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -2,6 +2,8 @@ package com.luv2code.test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -94,6 +96,46 @@ class MockAnnotationTest {
 		assertNotNull(applicationService.checkNull(
 				studentOne.getStudentGrades().getMathGradeResults()),
 				"Object should not be null");
+	}
+	
+	
+	@Test
+	@DisplayName("Throw Runtime Exception")
+	public void throwRuntimeError()
+	{
+		CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+		
+		doThrow(new RuntimeException())
+			.when(applicationDao).checkNull(nullStudent);
+		
+		assertThrows(RuntimeException.class, 
+				() -> {
+					applicationService.checkNull(nullStudent);
+				}
+				);
+		
+		verify(applicationDao, times(1)).checkNull(nullStudent);
+	}
+	
+	@Test
+	@DisplayName("Multiple Stubbing")
+	public void stubbingConsecutiveCalls()
+	{
+		CollegeStudent nullStudent = (CollegeStudent) context.getBean("collegeStudent");
+		when(applicationDao.checkNull(nullStudent))
+			.thenThrow(new RuntimeException())
+			.thenReturn("Do not throw exception second time");
+		
+		assertThrows(RuntimeException.class,
+				() -> {
+					applicationService.checkNull(nullStudent);					
+		});
+		
+		assertEquals("Do not throw exception second time", 
+				applicationService.checkNull(nullStudent));
+		
+		verify(applicationDao,times(2)).checkNull(nullStudent);
+		
 	}
 
 }
